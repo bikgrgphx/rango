187f622318684ab18f201d700d9d9250e81f314f
bikgrgphx <bikgrgphx@hotmail.com>
2015-10-11 18:16:59 +0300

    Simple views

--- TangoWithDjango/rango/views.py
+++ TangoWithDjango/rango/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says hello world!Go to about Page<a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("Rango says you are at about page. Go to indexx page <a href='/rango/'>Index<a>")
